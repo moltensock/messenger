c781fa32732108a908a060033631b02bd81b1a43
Sasha Fedosimova <sasha.fedosimova@mail.ru>
2021-09-14 00:45:13 +0300

    14.09

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
+ListOfMessages = []
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    return 'Messenger is running! ' \
+           '<br> <a href="/status">Кто нажмёт тот пидор</a>'
+
+
+@app.route('/status')
+def status():
+    return {
+        'messages_count': len(ListOfMessages)
+    }
+
+
+@app.route("/api/Messenger", methods=['POST'])
+def SendMessage():
+    message = request.json
+    print(message)
+    ListOfMessages.append(message)
+    print(message)
+    message_text = f"{message['UserName']} <{message['TimeStamp']}>: {message['MessageText']}"
+    print(f"Всего сообщений: {len(ListOfMessages)}. Посланное сообщение: {message_text}")
+    return f"Сообщение отправлено! Всего сообщений: {len(ListOfMessages)} ", 200
+
+
+@app.route("/api/Messenger/<int:id>")
+def GetMessage(id):
+    print(id)
+    if 0 <= id < len(ListOfMessages):
+        print(ListOfMessages[id])
+        return ListOfMessages[id], 200
+    else:
+        return "Not found", 400
+
 
 if __name__ == '__main__':
     app.run()
