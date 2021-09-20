0c689a1d3de7d56509fe2251af3cb16047108e7e
Sasha Fedosimova <sasha.fedosimova@mail.ru>
2021-09-20 20:47:02 +0300

    20.09

--- sender.py
+++ sender.py
@@ -1,7 +1,6 @@
-from urllib import response
 import requests
 
-requests.get('http://127.0.0.1:5000/status')
+response = requests.get('http://127.0.0.1:5000/status')
 print(response.status_code)
 print(response.text)
 print(response.json())
